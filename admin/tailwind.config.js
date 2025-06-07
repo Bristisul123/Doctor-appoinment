ec59763390a9a50c7c65eb573b149f0e7be60096
Zarin <ztasnim263@gmail.com>
2025-06-07 23:59:13 +0600

    Doctor List Done

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -3,9 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'primary':'#5F6FFF'
-      }
+      colors: {
+        primary: "#5F6FFF",
+      },
     },
   },
   plugins: [],
